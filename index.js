e109f388bbbe3a1781dbe590477afbb03e05f42f
Robert1GA <roberts.2015@yahoo.co.uk>
2017-02-22 10:44:43 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+// var session = require('express-session');
+// var passport = require('./config/ppConfig');
+var flash = require('connect-flash');
+var app = express();
+
+app.use(require('morgan')('dev'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public/'));
+
+app.use(flash());
+
+app.use(function(req, res, next) {
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+
+var server = app.listen(process.env.PORT || 3000);
+
+module.exports = server;
