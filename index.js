0a5eb7922c60b2edec2b9a585e181fbea40a0563
Robert1GA <roberts.2015@yahoo.co.uk>
2017-02-22 11:41:11 -0800

    adding to structure of pages

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
-// var session = require('express-session');
+var session = require('express-session');
 // var passport = require('./config/ppConfig');
 var flash = require('connect-flash');
 var app = express();
+var mongoose = require('mongoose');
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
@@ -13,12 +14,6 @@ app.use(ejsLayouts);
 app.use(express.static(__dirname + '/public/'));
 
 app.use(flash());
-
-app.use(function(req, res, next) {
-  res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user;
-  next();
-});
 
 app.get('/', function(req, res) {
   res.render('index');
