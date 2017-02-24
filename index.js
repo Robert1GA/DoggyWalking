a6341d992c4712e192859fd52064a0ababae4060
Robert1GA <roberts.2015@yahoo.co.uk>
2017-02-24 09:47:58 -0800

    quick cleanup before first push to heroku

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 var express = require('express');
-// var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-// var passport = require('./config/ppConfig');
 var flash = require('connect-flash');
 
 //**** JSON web token dependencies, including a secret key to sign the token *****
@@ -12,10 +10,6 @@ var jwt = require('jsonwebtoken');
 var secret = process.env.JWT_SECRET;
 var app = express();
 app.use(require('morgan')('dev'));
-// app.use(flash());
-// var http = require('http').Server(app);
-// app.set('view engine', 'ejs');
-
 var path = require('path');
 
 // ***** mongoose models and connection *****
@@ -23,13 +17,10 @@ var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/doggywalk');
 var User = require('./models/user');
 
-
 // decode POST data in JSON and URL encoded formats //
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(__dirname + '/public/'));
-
 
 // AUTH HERE
 app.use('/users', require('./controllers/users'));
@@ -43,6 +34,7 @@ app.use(function (err, req, res, next) {  // middleware to check if expressJWT a
     res.status(401).send({ message: 'You need an authorization token to view this information.' });
   }
 });
+
 // POST /api/auth - if authenticated, return a signed JWT
 app.post('/api/auth', function(req, res) {
   User.findOne({ email: req.body.email }, function(err, user) {
@@ -60,7 +52,6 @@ app.post('/api/auth', function(req, res) {
     return res.send({ user: user, token: token });
   });
 });
-
 
 // Routes
 app.use('/api', require('./controllers'));
